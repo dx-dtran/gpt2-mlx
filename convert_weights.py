864970c518c70f3568ceebef05a6d7a59296e7d7
daniel <dxdtran@outlook.com>
2023-12-08 20:11:05 -0800

    add script to convert pytorch weights to mlx

---
+++ convert_weights.py
@@ -0,0 +1,10 @@
+import numpy as np
+import torch
+
+
+if __name__ == "__main__":
+    state_dict = torch.load("gpt2.bin")
+    for key, value in state_dict.items():
+        print(f"Layer: {key}")
+        print(f"Shape: {value.shape}")
+    np.savez("gpt2.npz", **{k: v.to(torch.float16).numpy() for k, v in state_dict.items()})
