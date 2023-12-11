96db160378bb2adad73261d9e1498594baa9f4e2
daniel <dxdtran@outlook.com>
2023-12-11 13:05:01 -0800

    make train code more modular

--- train.py
+++ train.py
@@ -13,8 +13,8 @@ from dataclasses import dataclass
 
 @dataclass
 class TrainConfig:
-    batch_size: int
     num_iters: int
+    batch_size: int
     grad_accumulation_steps: int
     max_lr: float
     min_lr: float
@@ -57,88 +57,130 @@ class DataLoader:
                 s = 0
 
 
-def get_learning_rate(it, lr, min_lr, warmup_iters, lr_decay_iters):
-    if it < warmup_iters:
-        return lr * it / warmup_iters
-    if it > lr_decay_iters:
-        return min_lr
-    decay_ratio = (it - warmup_iters) / (lr_decay_iters - warmup_iters)
-    assert 0 <= decay_ratio <= 1
-    coeff = 0.5 * (1.0 + math.cos(math.pi * decay_ratio))
-    return min_lr + coeff * (lr - min_lr)
+class GPTTrainer:
+    def __init__(self, data_path, train_config: TrainConfig, model_config: GPTConfig):
+        self.data_path = data_path
+        self.data_loader = DataLoader(data_path, model_config.block_size)
+
+        # training hyperparameters
+        self.train_config = train_config
+        self.batch_size = train_config.batch_size
+        self.num_iters = train_config.num_iters
+        self.grad_accumulation_steps = train_config.grad_accumulation_steps
+        self.max_lr = train_config.max_lr
+        self.min_lr = train_config.min_lr
+        self.warmup_iters = train_config.warmup_iters
+        self.lr_decay_iters = train_config.lr_decay_iters
+
+        # initialize the model and optimizer
+        self.model_config = model_config
+        self.model = GPT(model_config)
+        self.optimizer = opt.AdamW(learning_rate=self.max_lr)
+        self.loss_and_grad_fn = nn.value_and_grad(self.model, self.model.loss)
+
+    def print_model_parameters(self):
+        mx.eval(self.model.parameters())
+        nparams = sum(x.size for k, x in tree_flatten(self.model.parameters()))
+        print(f"Training a custom GPT model with {nparams / 1e6:.3f} M parameters")
+
+    def print_progress(self, iteration_count, average_loss, tic):
+        toc = time.perf_counter()
+        print(
+            f"iter {iteration_count}: train loss {average_loss:.3f}, "
+            f"it/sec {1.0 / (toc - tic):.3f}, "
+            f"lr {self.optimizer.learning_rate:.4f}"
+        )
+        return toc
+
+    def get_learning_rate(self, it):
+        if it < self.warmup_iters:
+            return self.max_lr * it / self.warmup_iters
+        if it > self.lr_decay_iters:
+            return self.min_lr
+        decay_ratio = (it - self.warmup_iters) / (
+            self.lr_decay_iters - self.warmup_iters
+        )
+        assert 0 <= decay_ratio <= 1
+        coeff = 0.5 * (1.0 + math.cos(math.pi * decay_ratio))
+        return self.min_lr + coeff * (self.max_lr - self.min_lr)
+
+    def update_learning_rate(self, iteration_count):
+        current_lr = self.get_learning_rate(iteration_count)
+        self.optimizer.set_learning_rate(current_lr)
+
+    def process_batch(self, inputs, targets):
+        inputs, targets = map(mx.array, (inputs, targets))
+        loss, grads = self.loss_and_grad_fn(inputs, targets)
+        return loss, grads
+
+    def train(self):
+        self.print_model_parameters()
+
+        data_loader = DataLoader(self.data_path, self.model_config.block_size)
+        train_iterator = data_loader.get_batch_iterator(self.train_config.batch_size)
+
+        accumulated_grads = tree_map(
+            lambda x: mx.zeros_like(x), self.model.parameters()
+        )
+        accumulated_loss = 0.0
+        weight_per_step = 1.0 / self.train_config.grad_accumulation_steps
+        tic = time.perf_counter()
+
+        total_micro_batch_iters = (
+            self.train_config.num_iters * self.train_config.grad_accumulation_steps
+        )
+        full_iteration_count = 0
+        for it in range(total_micro_batch_iters):
+            inputs, targets = next(train_iterator)
+            loss, grads = self.process_batch(inputs, targets)
+
+            accumulated_grads = tree_map(
+                lambda acc, new: acc + new * weight_per_step,
+                accumulated_grads,
+                grads,
+            )
+            accumulated_loss += loss.item()
+
+            if (it + 1) % self.train_config.grad_accumulation_steps == 0:
+                self.update_learning_rate(full_iteration_count)
+                self.model.update(
+                    self.optimizer.apply_gradients(accumulated_grads, self.model)
+                )
+                accumulated_grads = tree_map(
+                    lambda x: mx.zeros_like(x), self.model.parameters()
+                )
+                average_loss = (
+                    accumulated_loss / self.train_config.grad_accumulation_steps
+                )
+                accumulated_loss = 0.0
+
+                mx.simplify(loss, self.model.parameters())
+                mx.eval(loss, self.model.parameters())
+                full_iteration_count += 1
+                tic = self.print_progress(full_iteration_count, average_loss, tic)
 
 
 def main(train_path):
-    config_args = dict(
-        n_layer=3, n_head=4, n_embd=256, vocab_size=50304, block_size=256, bias=True
+    model_config = GPTConfig(
+        n_layer=3,
+        n_head=4,
+        n_embd=256,
+        vocab_size=50304,
+        block_size=256,
+        bias=True,
     )
 
-    config = GPTConfig(**config_args)
-
-    num_iters = 300
-    batch_size = 4
-    grad_accumulation_steps = 16
-    max_lr = 1e-3
-    min_lr = 1e-4
-    warmup_iters = 0
-    lr_decay_iters = 300
-
-    model = GPT(config)
-
-    mx.eval(model.parameters())
-    nparams = sum(x.size for k, x in tree_flatten(model.parameters()))
-    print(f"Training a custom GPT model with {nparams / 1e6:.3f} M parameters")
-
-    optimizer = opt.AdamW(learning_rate=max_lr)
-    loss_and_grad_fn = nn.value_and_grad(model, model.loss)
-
-    data_loader = DataLoader(train_path, config.block_size)
-    train_iterator = data_loader.get_batch_iterator(batch_size)
-
-    inputs, targets = next(iter(train_iterator))
-    inputs, targets = map(mx.array, (inputs, targets))
-
-    _, initial_grads = loss_and_grad_fn(inputs, targets)
-
-    accumulated_grads = tree_map(lambda x: mx.zeros_like(x), model.parameters())
-    accumulated_loss = 0.0
-    weight_per_step = 1.0 / grad_accumulation_steps
-    tic = time.perf_counter()
-
-    total_micro_batch_iters = num_iters * grad_accumulation_steps
-    full_iteration_count = 0
-    for it in range(total_micro_batch_iters):
-        inputs, targets = next(train_iterator)
-
-        inputs, targets = map(mx.array, (inputs, targets))
-        loss, grads = loss_and_grad_fn(inputs, targets)
-
-        accumulated_grads = tree_map(
-            lambda acc, new: acc + new * weight_per_step, accumulated_grads, grads
-        )
-        accumulated_loss += loss.item()
-
-        if (it + 1) % grad_accumulation_steps == 0:
-            curr_lr = get_learning_rate(
-                full_iteration_count, max_lr, min_lr, warmup_iters, lr_decay_iters
-            )
-            optimizer.set_learning_rate(curr_lr)
-
-            model.update(optimizer.apply_gradients(accumulated_grads, model))
-            accumulated_grads = tree_map(lambda x: mx.zeros_like(x), model.parameters())
-            average_loss = accumulated_loss / grad_accumulation_steps
-            accumulated_loss = 0.0
-
-            mx.simplify(loss, model.parameters())
-            mx.eval(loss, model.parameters())
-            full_iteration_count += 1
-            toc = time.perf_counter()
-            print(
-                f"iter {full_iteration_count}: train loss {average_loss:.3f}, "
-                f"it/sec {1.0 / (toc - tic):.3f}, "
-                f"lr {curr_lr:.4f}"
-            )
-            tic = time.perf_counter()
+    train_config = TrainConfig(
+        num_iters=300,
+        batch_size=4,
+        grad_accumulation_steps=16,
+        max_lr=1e-3,
+        min_lr=1e-4,
+        warmup_iters=0,
+        lr_decay_iters=300,
+    )
+    trainer = GPTTrainer(train_path, train_config, model_config)
+    trainer.train()
 
 
 if __name__ == "__main__":
